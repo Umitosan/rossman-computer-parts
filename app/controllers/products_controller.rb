df8d189e349400b2f87730f50ae5c10f338d2bb0
Dominic Brown <yeahdom@gmail.com>
2017-06-30 14:29:02 -0700

    add feature add product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,23 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def new
+    @product = Product.new
+  end
 
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "( Product saved! )"
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :cost, :country)
+  end
 
 end
